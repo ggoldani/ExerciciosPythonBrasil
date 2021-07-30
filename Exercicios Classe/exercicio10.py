3dca0fcdbeee07775c6684a042b779368132bf97
ggoldani <tech.goldani@gmail.com>
2021-07-30 11:08:42 -0300

    melhorando o programa

--- Exercicios Classe/exercicio10.py
+++ Exercicios Classe/exercicio10.py
@@ -10,41 +10,65 @@ class BombaCombustivel:
 
     def abasterPorValor(self, valor_abastecido:float):
         litrosAbastecidos = valor_abastecido / self.valor_litro
-        self.quantidade_combustivel -= litrosAbastecidos
-        return print(f'Foram abastecidos {litrosAbastecidos:.2f} litros e sobraram {self.quantidade_combustivel:.2f} litros de {self.tipo_combustivel} na bomba.')
+        if litrosAbastecidos < self.quantidade_combustivel:
+            self.quantidade_combustivel -= litrosAbastecidos
+            return print(f'Foram abastecidos {litrosAbastecidos:.2f} litros e sobraram {self.quantidade_combustivel:.2f} litros de {self.tipo_combustivel} na bomba.')
+        else:
+            print('N�o h� combustivel suficiente.')
 
     def abastecerPorLitro(self, litros_abastecidos:float):
-       valor_abastecido = litros_abastecidos * self.valor_litro
-       self.quantidade_combustivel -= litros_abastecidos
-       return print(f'O valor a ser pago eh de R$ {valor_abastecido:.2f} e sobram {self.quantidade_combustivel:.2f} litros de {self.tipo_combustivel} na bomba.')
+        if litros_abastecidos < self.quantidade_combustivel:
+            valor_abastecido = litros_abastecidos * self.valor_litro
+            self.quantidade_combustivel -= litros_abastecidos
+            return print(f'O valor a ser pago eh de R$ {valor_abastecido:.2f} e sobram {self.quantidade_combustivel:.2f} litros de {self.tipo_combustivel} na bomba.')
+        else:
+            print('N�o h� quantidade de combustivel suficiente.')
 
     def alterarValorLitro(self, valor_litro):
         self.valor_litro = valor_litro
-        return print(f'Valor do litro atualizado, novo valor R$ {self.valor_litro:.2f}.')
+        return print(f'Valor do litro de {self.tipo_combustivel} atualizado, novo valor R$ {self.valor_litro:.2f}.')
 
     def alterarQuantidadeCombustivel(self, litros_adicionados):
         self.quantidade_combustivel += litros_adicionados
-        return print(f'Litros totais na bomba: {self.quantidade_combustivel:.2f} L.')
+        return print(f'Litros adicionados: {litros_adicionados}\nLitros totais na bomba: {self.quantidade_combustivel:.2f} L.')
 
-bomba = BombaCombustivel("Alcool", 4.09, 1000.00)
+tipo_combustivel = input('Informe o tipo de combustivel da bomba (Alcool ou Gasolina): ')
+valor_litro = float(input('Informe o valor por litro R$'))
+quantidade_combustivel = float(input('Quantidade de combustivel em estoque: '))
+bomba = BombaCombustivel(tipo_combustivel, valor_litro, quantidade_combustivel)
 
-valor_abastecido = float(input('Valor a ser abastecido: R$'))
-bomba.abasterPorValor(valor_abastecido)
+i = 1
 
-litros_abastecidos = float(input('Litros a serem abastecidos: R$'))
-bomba.abastecerPorLitro(litros_abastecidos)
+while True:
+    print(f'======= Cliente {i} =======')
 
-trocar_valor_litro = input(f'Senhor gerente, deseja alterar o valor do litro do combustivel atual S/N? ')
-if trocar_valor_litro in "Ss":
-    novo_valor_litro = float(input('Informe o novo valor: R$'))
-    bomba.alterarValorLitro(novo_valor_litro)
+    litro_ou_valor = input('Tipo de abastecimento:\n1 - Por valor\n2 - Por litro\n')
 
-alterar_quantidade_comustivel = input(f'Deseja alterar a quantidade atual de combustivel S/N? ')
-if alterar_quantidade_comustivel in "Ss":
-    quantidade_adicionada = float(input('Informe a quantidade de litros a ser adicionada: '))
-    if quantidade_adicionada < 0:
-       print('Eh proibida a retirada de combustivel.')
+    if litro_ou_valor == '1':
+        valor_abastecido = float(input('Valor a ser abastecido: R$'))
+        bomba.abasterPorValor(valor_abastecido)
+    elif litro_ou_valor == '2':
+        litros_abastecidos = float(input('Litros a serem abastecidos: '))
+        bomba.abastecerPorLitro(litros_abastecidos)
     else:
-        bomba.alterarQuantidadeCombustivel(quantidade_adicionada)
+        print('Opç�o inv�lida.')
 
-print('Finalizar programa.')
+    trocar_valor_litro = input(f'Deseja alterar o valor do litro do combustivel atual S/N? ')
+    if trocar_valor_litro in "Ss":
+        novo_valor_litro = float(input('Informe o novo valor: R$'))
+        bomba.alterarValorLitro(novo_valor_litro)
+
+    alterar_quantidade_comustivel = input(f'Deseja alterar a quantidade atual de combustivel S/N? ')
+    if alterar_quantidade_comustivel in "Ss":
+        quantidade_adicionada = float(input('Informe a quantidade de litros a ser adicionada: '))
+        if quantidade_adicionada < 0:
+            print('Eh proibida a retirada de combustivel.')
+        else:
+            bomba.alterarQuantidadeCombustivel(quantidade_adicionada)
+
+    encerrar = input('Encerrar? S/N ')
+    if encerrar in "Ss":
+        break
+    i += 1
+
+print('Programa finalizado.')
