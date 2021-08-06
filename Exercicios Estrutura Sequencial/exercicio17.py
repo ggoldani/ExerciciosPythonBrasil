70bef9fdbdccc538eba1e29c41b7c6ac652ede48
ggoldani <tech.goldani@gmail.com>
2021-08-05 21:25:01 -0300

    lista python brasil exercicio 17 estrutura sequencial

--- Exercicios Estrutura Sequencial/exercicio17.py
+++ Exercicios Estrutura Sequencial/exercicio17.py
@@ -1,20 +1,43 @@
 import math
 
-area = eval(input('Digite a area a ser pintada em m²: '))
-area_com_folga = area * 1.1
+area = eval(input('Informe a área a ser pintada (em m²): '))
+area_a_ser_pintada = area * 1.1
+litros = area_a_ser_pintada / 6
+custo_lata = 80
+litro_lata = 18
+custo_galao = 25
+litro_galao = 3.6
 
-litro_tinta = area_com_folga / 6
-quantidade_lata = litro_tinta / 18
-quantidade_galao = litro_tinta / 3.6
-preco_lata = 80
-preco_galao = 25
+quantidade_apenas_lata = math.ceil(litros / litro_lata)
+custo_apenas_lata = quantidade_apenas_lata * custo_lata
 
-print(f'Usando apenas latas de 18L, sao necessarias {math.ceil(quantidade_lata)}, com custo de R${math.ceil(quantidade_lata)* preco_lata:.2f}. ')
-print(f'Usando apenas galoes de 3.6L, sao necessarios {math.ceil(quantidade_galao)}, com custo de R${math.ceil(quantidade_galao * preco_galao):.2f}.')
+if quantidade_apenas_lata == 1:
+    print(f'Usando apenas latas, será necessária {quantidade_apenas_lata} lata com custo de R${custo_apenas_lata:.2f}.')
+elif quantidade_apenas_lata > 1:
+    print(f'Usando apenas latas, será necessária {quantidade_apenas_lata} lata com custo de R${custo_apenas_lata:.2f}.')
 
-litros_que_sobram = litro_tinta % 18
-valor_total = math.floor(quantidade_lata) * preco_lata + math.ceil(litros_que_sobram / 3.6) * preco_galao
+quantidade_apenas_galao = math.ceil(litros / litro_galao)
+custo_apenas_galao = quantidade_apenas_galao * custo_galao
 
-print(f'Usando latas e galoes, sao necessarios {math.floor(quantidade_lata)} latas e {math.ceil(litros_que_sobram / 3.6)} galoes, com valor total de R${valor_total:.2f}.')
+if quantidade_apenas_galao == 1:
+    print(f'Usando apenas galões, será necessário {quantidade_apenas_galao} galão com custo de R${custo_apenas_galao:.2f}.')
+elif quantidade_apenas_galao > 1:
+    print(f'Usando apenas galões, serão necessários {quantidade_apenas_galao} galões com custo de R${custo_apenas_galao:.2f}.')
 
+quantidade_lata = math.floor(litros / litro_lata)
+litros_que_faltam = litros % litro_lata
+quantidade_galao = math.ceil(litros_que_faltam / litro_galao)
+custo_total = quantidade_lata * custo_lata + quantidade_galao * custo_galao
 
+print('Usando latas e galões ', end = '')
+if quantidade_lata == 1:
+    print(f'será necessária {quantidade_lata} lata ', end = '')
+elif quantidade_lata > 1:
+    print(f'serão necessárias {quantidade_lata} latas ', end='')
+
+if quantidade_galao == 1:
+    print(f'e {quantidade_galao} galão ', end='')
+elif quantidade_galao > 1:
+    print(f'e {quantidade_galao} galões ', end='')
+
+print(f'com custo total de R${custo_total:.2f}.')
