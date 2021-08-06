c923d557fb9ebeb821cca5e5cb8bc7f1e472ec03
ggoldani <tech.goldani@gmail.com>
2021-08-05 23:45:20 -0300

    usando upper() para evitar erro ao nao digitar nada, apenas pressionar enter

--- Exercicio Estr Decisao/exercicio03.py
+++ Exercicio Estr Decisao/exercicio03.py
@@ -1,10 +1,10 @@
-letra = input('Digite a lera correspondente ao seu sexo, sendo F para feminino e M para masculino: ')
+letra = input('Digite a lera correspondente ao seu sexo, sendo F para feminino e M para masculino: ').upper()
 
 while True:
-    if letra in "Ff":
+    if letra == "F":
         print('Letra F digitada. Feminino.')
         break
-    elif letra in "Mm":
+    elif letra == "M":
         print('Letra M digitada. Masculino.')
         break
     else:
